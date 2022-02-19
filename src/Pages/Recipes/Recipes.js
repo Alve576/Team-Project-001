5cb39cc1526209f5fa22b0f329a5a6b1903d14df
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-02-19 12:07:58 +0600

    pull from main

--- src/Pages/Recipes/Recipes.js
+++ src/Pages/Recipes/Recipes.js
@@ -11,8 +11,9 @@ const Products = () => {
     }, [])
     return (
         <div className='container p-0' >
+            <h3>You Can Shop From Here.</h3>
             <ShopHeader></ShopHeader>
-            <h3>Clothes</h3>
+            <h3>Recipes</h3>
             <div className='row px-0 justify-content-around'>
                 {
                     products.map(product => <Recipe key={product.id} product={product}></Recipe>)
